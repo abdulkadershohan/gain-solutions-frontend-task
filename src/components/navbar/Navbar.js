29a2b73b670225cede5c761525437339ba6f96d6
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-08-11 11:57:25 +0600

    init

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import SearchIcon from '../../assets/svg/SearchIcon'
+import './Navbar.css'
+export default function Navbar() {
+    return (
+        <nav className={'navbar navbar-expand-lg'} >
+            <div className="container-fluid  ">
+                <a className="navbar-brand logo  text-white " href="#home">LOGO</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse gap-2 " id="navbarSupportedContent">
+                    <form className='form-search' >
+                        <input
+                            className='search '
+                            type="search"
+                            placeholder="Search by Title or Brand"
+                            aria-label="Search"
+                            id='search'
+                        />
+                        <span className='search-icon'>
+                            <SearchIcon />
+                        </span>
+                    </form>
+                    {/* <form>
+                        <input
+                            className='search '
+                            type="search"
+                            placeholder="Search by Title or Brand"
+                            aria-label="Search"
+                            id='search'
+                        />
+                    </form> */}
+
+
+                    {/* <div class="input-group">
+                        <input
+                            className='search '
+                            type="search"
+                            placeholder="Search by Title or Brand"
+                            aria-label="Search"
+                            id='search'
+                        />
+                        <span className="input-group-addon">
+                            <SearchIcon />
+                        </span>
+                    </div> */}
+
+                    <button className=' add-product-btn py-2 px-3 ' type='submit'>Add Product</button>
+                </div>
+            </div>
+        </nav >
+    )
+}
