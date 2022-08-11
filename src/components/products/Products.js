94cd0cd3f47c54e040b9c2828e3b1e53f7920d6b
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-08-11 23:15:55 +0600

    dropdown

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,18 +1,46 @@
 import React from 'react'
 import { HeaderTitle } from '../../utilitty'
+import './Products.css'
 
 export default function Products() {
     return (
         <div className="container">
-            <div className="row">
+            <div className="row align-items-center">
                 <div className="col-6">
                     <HeaderTitle title={'ALL Products'} />
 
                 </div>
                 <div className="col-6">
+                    <div className="d-flex align-items-end flex-column">
+                        <form>
+                            <label className='dropdown-label  ' htmlFor="cars">Sort by:</label>
+                            <select name="cars" id="cars" className='dropdown-product'>
+                                <option value="All products">All products</option>
+                                <option value="Best value">Best value</option>
+                                <option value="Best Performance">Best Performance</option>
+                                <option value="audi">Best Camera</option>
+                            </select>
+                            <br />
 
+                        </form>
+                    </div>
                 </div>
             </div>
+            <div className="row py-4">
+                <div className="col-3">
+                    <p className='table-title'>Model</p>
+                </div>
+                <div className="col-3">
+                    <p className='table-title'>Ram/Rom</p>
+                </div>
+                <div className="col-3">
+                    <p className='table-title'>Tag</p>
+                </div>
+                <div className="col-3">
+                    <p className='table-title'>Price</p>
+                </div>
+            </div>
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
